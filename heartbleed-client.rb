911f430b23cdf74cfc39394c4f37e1b168e5e10a
krishpranav <krisna.pranav@gmail.com>
2021-07-16 10:41:20 +0530

    heartbleed client

--- heartbleed-client.rb
+++ heartbleed-client.rb
@@ -6,6 +6,9 @@ require 'openssl'
 begin
     port = (ARGV[0] || 4443).to_i
     socket = TCPSocket.new('localhost', port)
+    ssl = OpenSSL::SSL::SSLSocket.new(socket)
+    ssl.sync_close = true
+    ssl.connect
 
 rescue OpenSSL::SSL::SSLError
 end
